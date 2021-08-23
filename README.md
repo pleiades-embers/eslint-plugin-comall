4b58405f49d163a326a44cfe09b972ee701306d7
aoarashi1988 <dodolo321@sina.com>
2021-08-23 16:48:24 +0800

    feat: 添加规则：no-expression-in-jsx-props

---
+++ README.md
@@ -0,0 +1,46 @@
+# eslint-plugin-comall
+
+eslint plugin with comall private rules .
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-comall`:
+
+```sh
+npm install eslint-plugin-comall --save-dev
+```
+
+## Usage
+
+Add `comall` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "comall"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "comall/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
---
+++ tests/lib/rules/no-expression-in-jsx-props.js
@@ -0,0 +1,103 @@
+/**
+ * @fileoverview no expression pass to jsx props, except literal or member expression 
+ * @author yefeng
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-expression-in-jsx-props"),
+  RuleTester = require("eslint").RuleTester
+
+  const parserOptions = {
+      ecmaVersion:2015,
+      ecmaFeatures: {
+        jsx: true
+      }
+    }
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({parserOptions});
+ruleTester.run("no-expression-in-jsx-props", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+    {code:'<div foo={foo} />'},
+    {code:'<div foo={1} />'},
+    {code:'<div foo="bar" />'},
+    {code:'<div foo={false} />'},
+    {code:'<div foo />'},
+    {code:'<div foo={this} />'},
+    {code:'<div foo={bar.baz} />'},
+  ],
+
+  invalid: [
+    {
+      code:"<div foo={[]} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code: "<div foo={bar === baz} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={{}} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={bar = baz} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={bar && baz} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={new Bar()} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={() => {}} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={function(){}} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={/bar/} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={<div/>} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={<></>} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={bar ? baz: barzz} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={``} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={typeof bar} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={bar()} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+    {
+      code:"<div foo={bar.bind(baz)} />",
+      errors:[{messageId:'defaultMsg'}],
+    },
+  ],
+});
